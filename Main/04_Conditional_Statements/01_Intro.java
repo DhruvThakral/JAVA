2af9b3a0550384cb5002dc71ace94dbb378a3252
Dhruv <dhruvthakral26@gmail.com>
2024-01-18 20:40:00 +0530

    First Commit

--- Main/04_Conditional_Statements/01_Intro.java
+++ Main/04_Conditional_Statements/01_Intro.java
@@ -1,12 +1,30 @@
+// class LPU {
+//     public static void main(String args[]){
+//         int a =5;
+//         int b= 7;
+//         if(a>b){
+//             System.out.println("A is greater.");
+//         }
+//         else{
+//             System.out.println("B is greater.");
+//         }
+//     }
+// }
+
+//Write a java program that determines if you can skydive. Skydive can be done by a person whose age is greater than or equal to 18 years and less than or equal to 60 years.
+
 class LPU {
     public static void main(String args[]){
-        int a =5;
-        int b= 7;
-        if(a>b){
-            System.out.println("A is greater.");
+        int age = 20;
+        
+        if(age >= 18  && age <= 60){
+            System.out.println("Yes, You can skydive.");
         }
         else{
-            System.out.println("B is greater.");
+            System.out.println("No, You can't skydive.");
         }
     }
 }
+
+
+
