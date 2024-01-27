99ac812ee0326920b8620579a0bc8a73478ba3d5
Dhruv <dhruvthakral26@gmail.com>
2024-01-27 12:19:35 +0530

    Java Day 6

--- Main/05_Loops/03_for.java
+++ Main/05_Loops/03_for.java
@@ -1,13 +1,27 @@
 
+// class LPU {
 
-class LPU {
+//     public static void main(String args[]){
 
-    public static void main(String args[]){
+//         int i=0;
+//         for(i = 1; i < 109; i++){
+//             System.out.println("Ram Ram " + i);
+//         }
+//     }
 
-        int i=0;
-        for(i = 1; i < 109; i++){
-            System.out.println("Ram Ram " + i);
-        }
-    }
-    
-}
+// }
+
+//-------------------------Nested Loops:
+        // class LPU {
+        //     public static void main(String args[]) {
+        //         for (int i = 0; i < 5; i++) {
+        //             System.out.print(" Outer -> " + i);
+        //             for (int j = 0; j < 5; j++) {
+        //                 System.out.print(" Inner -> " + j);
+        //             }
+        //             System.out.println("\n");
+        //         }
+        //     }
+        // }
+
+
---
+++ Main/05_Loops/04_patterns.java
@@ -0,0 +1,75 @@
+//-----------------------Pattern Printing
+
+// i=0     * * * *   -->4 stars
+// i=1     * * * *   -->4 stars
+// i=2     * * * *   -->4 stars
+// i=3     * * * *   -->4 stars
+// Use outer loop for rows and inner loop for columns
+
+// class LPU {
+//     public static void main(String args[]){
+//         for(int i=0;i<4;i++){
+//             for(int j=0;j<4;j++){
+//                 System.out.print(" * ");
+//             }
+//             System.out.print("\n");
+//         }
+
+//     }
+// }
+
+//i=1      *           j=1
+//i=2      * *          j=2
+//i=3      * * *        j=3
+//i=4      * * * *      j=4
+// class LPU {
+//     public static void main(String args[]) {
+//         for (int i = 0; i < 4; i++) {
+//             for (int j = 0; j <= i; j++) {
+//                 System.out.print(" * ");
+//             }
+//             System.out.print("\n");
+//         }
+//     }
+// }
+
+//      0
+//      0 1
+//      0 1 2 
+//      0 1 2 3
+//      j ki values print ho rhi hai jitna bhi mera i hai
+// class LPU {
+//     public static void main(String args[]) {
+//         for (int i = 0; i < 4; i++) {
+//             for (int j = 0; j <= i; j++) {
+//                 System.out.print(j + " ");
+//             }
+//             System.out.print("\n");
+//         }
+//     }
+// }
+
+// * * * * *
+// * * * *
+// * * *
+// * *
+// *
+// class LPU {
+//     public static void main(String args[]) {
+//         for (int i = 5; i > 0; i--) {
+//             for (int j = i; j > 0; j--) {
+//                 System.out.print(" * ");
+//             }
+//             System.out.print("\n");
+//         }
+//     }
+// }
+
+//              *           
+//          *   *   *
+//      *   *   *   *   *
+//  *   *   *   *   *   *   *
+
+
+
+
