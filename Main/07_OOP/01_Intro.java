862ea86e64de364cd085d33a3346084949328631
Dhruv <dhruvthakral26@gmail.com>
2024-02-02 20:56:34 +0530

    Day-10

---
+++ Main/07_OOP/01_Intro.java
@@ -0,0 +1,39 @@
+// OOP: Object Oriented Programming
+// Class is a blueprint of an object.
+
+// class Add{
+
+//     public int sum(int num1 , int num2){
+//         int result = num1 + num2;
+//         return result;
+//     }
+
+//     public double sum(double num1, double num2){
+//         return num1+num2;
+//     }
+
+//     public int sum(int num1,int num2,int num3){
+//         return num1+num2+num3;
+//     }
+// }
+
+// class LPU{
+//     public static void main(String args[]){
+//         int n1=5;
+//         int n2=10;
+//         int n5 = 12;
+
+//         double n3=1.1;
+//         double n4=2.2;
+        
+
+//         // Creating instance of class 
+//         Add obj1 = new Add(); // new keyword create the object in heap memory // Here obj1 is reference variable of that class which we have to get accessed.
+//        double output = obj1.sum(n1,n2,n5);
+//        System.out.println(output);
+        
+//     }
+// }
+
+
+
