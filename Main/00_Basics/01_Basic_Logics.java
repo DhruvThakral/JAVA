d11caa7c3f4ebf609056ade87c480e72817e2478
Dhruv <dhruvthakral26@gmail.com>
2024-02-06 14:13:08 +0530

    Logics

---
+++ Main/00_Basics/01_Basic_Logics.java
@@ -0,0 +1,25 @@
+//Important Logics:
+
+// For finding the last digit of number:   number % 10 ;
+
+// For Swaping the numbers(num1 , num2) using Bitwise XOR:   
+//         Step 1: num1 = (num1) ^ (num2);
+//         Step 2: num2 = (num1) ^ (num2);
+//         Step 3: num1 = (num1) ^ (num2);
+
+// Ex.: x=4 , y=5 ==> Find x-- * y--  ==>Answer: 20
+
+// main : Serves as the entry point of the program and executes the code.
+
+//In  Java, a string is enclosed in double quotes and not in single quotes.
+
+//In Java , 
+// int numberSeven = 0b111 ==> In Binary
+// int  numberNine = 011  ==> Octal
+// int  numberZero = 0x0  ==>Hexadecimal ==> Both uppercase and lowercase letters can be used when representing hex values like 60 can be  writtern as : 0xA0  or   0Xa0
+
+// Data type  ==> default ==0            Wrapper Class ==> default == null
+// int                                      Integer         both can be used
+// Same for others data type.
+
+//If two or more operators of equal precedence are used , the expression evaluation happens from left to right.
