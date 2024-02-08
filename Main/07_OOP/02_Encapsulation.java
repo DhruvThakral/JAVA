a3ce949cbc6aedf98fdf6e22c25c81e782e8a9ff
Dhruv <dhruvthakral26@gmail.com>
2024-02-08 19:08:41 +0530

    Day  11

--- Main/07_OOP/02_Encapsulation.java
+++ Main/07_OOP/02_Encapsulation.java
@@ -1,34 +1,42 @@
-class Account {
+class Account{
 
-    // Hiding the data using private
     private String accName = "Hello";
     private int accBalance = 100;
     private int accNumber = 1234;
 
-    public int getAccBalance() {
+    public int getAccBalance(){
         return accBalance;
     }
 
-    public void setAccBalance(int val) {
+    public void setAccBalance(int val){
         accBalance = val;
     }
+
+    public String getAccName(){
+        return accName;
+    }
+
+    public void setAccName(String val){
+        accName = val;
+    }
 }
 
-// private variables are visible only in
-
-class LPU {
+class LPU{
     public static void main(String args[]){
-        // Encapsulation 
+        //Encapsulation
 
-        Account a1 = new Account(); 
-        System.out.println()
+        Account a1 = new Account();
+
+        System.out.println("Old Balance : " + a1.getAccBalance());
+        System.out.println("Old Name : " + a1.getAccName());
+
         a1.setAccBalance(50);
-        System.out.println(a1.accBalance);
- 
-       
+        a1.setAccName("World");
+
+        System.out.println("New Balance : " + a1.getAccBalance());
+        System.out.println("New Name : " + a1.getAccName());
 
     }
 }
 
-
-// Encapsulation is a process of wrapping  data members and 
+//Encapsulation is a process of wrapping data members and member functions together in a single unit
