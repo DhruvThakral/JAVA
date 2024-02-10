f452eafedc34cfdc3c2909884dd16ef0b3120906
Dhruv <dhruvthakral26@gmail.com>
2024-02-10 17:21:50 +0530

    Day 12

--- Main/07_OOP/05_Constructors.java
+++ Main/07_OOP/05_Constructors.java
@@ -1,29 +1,82 @@
 class Person{
+
     public String name;
     public int age;
+    public int mob;
 
-// I have a method which has the same name as of the class name and does not have any return type and this method is executed when we create an object autometically it will come.
-// Why we use this? :==>> Initializing the variables 
-public Person(){
-    System.out.println("From Constructor method");
-    name = "Hello";
-    age = 20;
-}
-// For each object default constructor is called once. 
+    //default constructor
+    public Person(){
+        System.out.println("In Default constructor");
+    }
+
+    public Person(String name, int age){
+        System.out.println("In Param constructor 1");
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person(String name, int age, int mob){
+        System.out.println("In Param constructor 2");
+        this.name = name;
+        this.age = age;
+        this.mob = mob;
+    }
+
+    public Person(Person p){
+        this.name = p.name;
+        this.age = p.age;
+        this.mob = p.mob;
+    }
 
 
 }
 
 class LPU{
-    public static void main(String args[]){
+    public static void main(String a[]){
 
-        // Constructors
+        //Constructors
 
-        Person p1 = new Person();
+        Person p1 = new Person("Hello",11);
+
         Person p2 = new Person();
-        Person p3 = new Person();
 
-        // System.out.println(p1.name); 
-        // System.out.println(p2.name);
+        Person p3 = new Person("World", 22, 9999);
+
+        Person p4 = new Person(p3);
+
+        System.out.println(p1.age);
+        System.out.println(p2.age);
+
+        System.out.println(p3.name + " : " + p3.age + " : " + p3.mob);
+
+        System.out.println("From p4 : " + p4.name);
+
     }
 }
+
+
+/*
+    - A constructor is a block of code similar to a class.
+    - It is called when an instance of a class is created.
+    - Every time an object is created using new keyword, at least one constructor is called
+    - Java compiler provides a default constructor by default, this constructor is called is there is no constructor is available in the class
+
+    - Rules
+    - Constructor name must be same as its class name
+    - Constructor should have no erturn type
+ */
+
+
+
+
+
+
+
+
+//  Create a Java program to model a simple car rental system. The system should allow users to rent a car and display the details of rented cars.
+
+// Your task is to implement a Car class with the following specifications:
+
+// The Car class should have private instance variables for color, model, year, and rentalStatus.
+// It should have a constructor that initializes color, model, and year when a new Car object is created, and sets rentalStatus to false initially.
+// Implement getter methods to retrieve the values of the instance variables.
---
+++ Main/08_Strings/01_Intro.java
@@ -0,0 +1,8 @@
+class LPU{
+    public static void main(String args[]){
+
+        String myName = new String();
+
+        System.out.println(myName);
+    }
+}
